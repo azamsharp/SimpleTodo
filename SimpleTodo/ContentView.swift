e160a1dd337c61d9449a6f59dec85c449240de2b
Mohammad Azam <azamsharp@gmail.com>
2021-10-20 09:53:45 -0500

    Initial Commit

---
+++ SimpleTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleTodo
+//
+//  Created by Mohammad Azam on 10/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleTodo/SimpleTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoApp.swift
+//  SimpleTodo
+//
+//  Created by Mohammad Azam on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
